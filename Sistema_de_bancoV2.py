fb61664cb7f8da364d608e0502ecd7e2492359de
britowb24@gmail.com <britowb24@gmail.com>
2025-05-14 16:50:34 -0300

    Funções de usuário e conta corrente criadas. 40% do projeto feito.

---
+++ Sistema_de_bancoV2.py
@@ -0,0 +1,222 @@
+def registro_user(credencial): #DEF RESPONSAVEL POR CADASTRAR NOVOS USUARIOS.
+    novo_user = {} #nossos usuarios são registrados em chave-valor. Essa var irá alimentar o nosso registro de usuarios em chave-valor.
+    novo_user['nome'] = str(input('Digite seu nome').capitalize()) 
+    while True:
+        try:
+            novo_user['Nascimento'] = int(input('Digite seu nascimento DD/MM/AAAA =>\n').replace('/','').replace(' ',''))
+            break
+        except ValueError:
+            print('Apenas números, por favor.')
+    #cpf = input('Digite seu cpf =>\n').replace('-','').replace('-','').replace(' ','')
+    novo_user['cpf'] = str(credencial)# Aqui eu resgato o CPF que já foi informado e verificado.
+    print('Agora seu endereço =>\n')
+    novo_user['endereco'] = registro_endereco() #Aqui alimentaremos a chave:valor endereço através da função registro_endereco. 
+    conta = []
+    novo_user['contas'] = conta
+    return novo_user #Retornamos a variavel que só existe na def para que o registro possa absorver os dados tratados.
+
+def registro_endereco(): #Com essa def vamos tratar os dados do endereço
+    campos_endereco = ['rua', 'numero', 'bairro', 'cidade', 'estado']
+    address = []
+    for registro in campos_endereco: #Variável registro percorre variável campos_endereço
+        if registro == 'estado': #Se no momento do loop registro for estado, então:
+            while True: #aqui trataremos para que os dados atendam os critérios que queremos.
+                estado = input('Digite a sigla do Estado =>\n').upper() 
+                if len(estado) == 2:
+                    address.append(estado)
+                    break
+                else:
+                    print('Apenas a sigla, por favor.')
+        elif registro == 'numero': #Se o registro, no momento do loop for numero
+            while True: #LOOP em que tenta converter o input em integral. Só sai daqui quando o resultado for numeral.
+                try:
+                    address.append(int(input('Digite o numero da sua casa =>\n')))
+                    break
+                except ValueError:
+                    print('Apenas números.')
+        else: 
+            address.append(input(f'Insira a(o) {registro} =>\n').capitalize()) #Se não houverem exceções a serem tratadas então só armazene o dado da vez.
+    address = f'{address[0]}, {address[1]} - {address[2]} - {address[3]}/{address[4]}'
+    print(address)
+    return address
+
+def abrir_conta():  #contabilidade += len(account['contas']) # Atualizar a quantidade de contas existentes.
+    conta_nova =[]
+    ag = '0001'
+    print('Sua agência será 0001')
+    conta_usuario = input('Digite uma identificacao para a conta')
+    global contabilidade
+    contabilidade += 1
+    conta_id = str(contabilidade)  # Vou atualizar a variavel responsavel pelo id das contas, o novo numero é o identificador da nova conta. Converto para string pois assim posso criar lista
+    print(f'Numero da conta: {conta_id}') 
+    conta_nova.append(ag) #AGENCIA
+    conta_nova.append(conta_id) #numero da conta
+    conta_nova.append(conta_usuario) # USUARIO
+    return conta_nova    
+        
+def user_loggin(credencial_encontrada): #(Aqui verificamos dados com o cpf escolhido. Os dados verificados são da var user.)
+    print(credencial_encontrada['cpf']) #Testar o retorno da variavel na def
+    print(f'''
+{user} Var user.
+{credencial_encontrada} Var credencial_encontrada
+{credencial_encontrada['cpf']}''' ) #Testar o retorno da var na def
+    while True: # RODA UM LOOP PARA QUE POSSA ALTERNAR ENTRE OPÇÕES
+        opcoes = int(input(''' 
+    1 - Listar contas    
+    2 - Abrir conta
+    3 - Sair
+    => '''))     # Ação que tomaremos uma vez que conseguimos autenticar os dados
+        if opcoes == 1: #LISTAR CONTAS
+            for i, items in enumerate(credencial_encontrada['contas']): #iterar valor de índice e valores da chave:valor 'conta'.
+                i += 1
+                if items: #Se houver alguma coisa 
+                    print(f'\n{i} - Agência: {items[0]} Conta: {items[1]}. Usuário: {items[2]}') #Mostra
+                else:
+                    print('Não existem contas')
+        elif opcoes == 2: # CRIAR CONTA
+                credencial_encontrada['contas'].append(abrir_conta())
+                print('Conta criada com sucesso')
+        elif opcoes == 3: #SAIR
+            break
+        else:
+            print('opção não encontrada. Digite o numero referente a opção desejada.')
+
+conta = []
+user = [{'nome': 'nome', 'Nascimento': 'data', 'cpf': 'cpf', 'endereco': 'endereço', 'contas':conta,}] #Nosso registro de usuários alimentado pela def registro_user
+contabilidade = len(conta) #Atualizador das contas
+while True:
+    escolha = int(input('Escolha 1 para logar e 2 para se registrar'))
+    if escolha == 1:
+        try:
+            credencial = input('Insira seu cpf').replace('-', '').replace('.','')#strip só remove do inicio ou fim. Replace é a melhor opção.
+            credencial_encontrada = next((usuario for usuario in user if usuario['cpf'] == credencial), None) #next busca o primeiro resultado, o for percorre toda a variável. 
+            if credencial_encontrada is not None: 
+                print(credencial_encontrada)
+                user_loggin(credencial_encontrada) #Chamamos o usuário logado
+            else:
+                print('CPF não encontrado.')
+        except ValueError:
+            print('Somente números, por favor.')
+    elif escolha == 2: #REGISTRAR. Primeiro verifico o CPF e se não existir eu permito o registro.
+        print(user)
+        credencial = input('Insira seu cpf').replace('-', '').replace('.','')#Replace para evitar a fadiga de quem vai digitar o cpf
+        if credencial.isdigit(): #Aqui verifico se é só número mesmo para evitar erro.
+            credencial_encontrada = next((usuario for usuario in user if usuario['cpf'] == credencial), None) #next busca o primeiro resultado, o for percorre toda a variável. 
+            if credencial_encontrada is not None: #Então eu verifiquei se o cpf já não existe para impedir sobreposições. 
+                print('Cliente já cadastrado.')
+                continue
+            else: #Como não tem, eu deixo registrar. Mas já envio credencial que tem o cpf armazenado
+                user.append(registro_user(credencial))  #A var retornada por registro_user é utilizada para preencher o responsável por manter o registro dos usuários.
+        else:
+            print('Apenas números, por favor.')
+        #print(user[0]) Como usamos .append não sobrescrevemos o primeiro valor. 
+        #print(user[1]) A partir daqui será incrementado os novos registros.
+        print(user)
+    else:
+        print('Opção inválida')
+
+
+    
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+
+conta = [{id:('ag','conta', 'usuario')}] #tuplas pois são dados, não queremos que sejam mutáveis.
+
+#Dividir todas as funções do sistema em defs. O retorno e o nome das variáveis é de meu critério
+
+#SAQUE DEVERÁ SER KEYWORD ONLY . saldo(valor = valor, saldo = saldo)
+#Sugestão de argumento é checar saldo e ver valor.
+# IDEIA MINHA: Vamos usar o módulo do extrato para retornar tanto para saque como para depósito.
+#SUGESTÃO DE RETORNO DO SAQUE: SALDO ATUAL E EXTRATO.
+
+#Função depósito deve receber argumentos positional only.
+#Sugestão de argumentos: saldo, valor, extrato.
+#Sugestão de retorno: saldo e extrato.
+
+#EXTRATO DEVE RECEBER ARGUMENTOS POR POSIÇÃO E NOME.
+# Argumentos posicionais: saldo
+# Argumntos nomeados: extrato
+
+#Criar usuário e conta corrente. E pode fazer mais funções, se quiser. Como listar ou inativar conta.
+
+#Criar usuário (cliente)
+#Armazenar usuários em uma lista. Um usuário é composto por: nome, data de nascimento, cpf e endereço.
+# O endereço é uma string com o formato: logradouro, numero - bairro - cidade/sigla estado.
+# Deve ser armazenado somente os números do cpf. (SEM CARACTERES ESPECIAIS)
+
+# Criar conta corrente
+
+#O programa deve armazenar contas em uma lista
+#Conta é composta por ag, numero da conta e usuário
+#ag padrão 0001. Numero da conta é sequencial a partir de 1
+#O usuário pode ter diversas contas (vamos criar um limitador modulado )
+#Uma conta só pode pertencer a um usuário
+#Se não houver um usuário deve ser impossível criar a conta.
+#Se houver um usuário com o cpf informado, vincule.
+
+#Usemos chave-valor dentro da lista.
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
